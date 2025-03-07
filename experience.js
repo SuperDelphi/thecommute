613c9033f5fb4dd267a3375cc32458a434825fb5
SuperDelphi <superdelphi@pm.me>
2025-03-07 22:38:19 +0100

    Added basic functions for managing stamps

--- experience.js
+++ experience.js
@@ -1,8 +1,32 @@
-const collectedStamps = [];
+let collectedStamps = [];
+let stamps = [];
 
-function init() {
+document.addEventListener("DOMContentLoaded", init);
+
+async function init() {
     const savedStamps = localStorage.getItem("stamps");
     if (savedStamps) {
         collectedStamps = JSON.parse(savedStamps);
     }
+
+    stamps = await fetchStamps();
+}
+
+function navigateToPage(page) {
+    document.querySelectorAll(".page").forEach(p => p.classList.remove("active"));
+    document.getElementById(page).classList.add("active");
+}
+
+async function fetchStamps() {
+    return await fetch("/stamps.json")
+        .then(response => response.json());
+}
+
+function collectStamp(stampId) {
+    if (collectedStamps.includes(stampId) || !stamps.find(stamp => stamp.id === stampId)) {
+        return;
+    }
+    console.log("Adding stamp", stampId);
+    collectedStamps.push(stampId);
+    localStorage.setItem("stamps", JSON.stringify(collectedStamps));
 }
