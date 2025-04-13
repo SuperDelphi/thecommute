83e5d2d72209813c07dadbb693b2392544b44ce2
SuperDelphi <44942598+SuperDelphi@users.noreply.github.com>
2025-04-13 16:05:54 +0200

    Changed the QR lib to one that scans "inverted" QR codes

--- experience.js
+++ experience.js
@@ -1,4 +1,4 @@
-import Scanner from "./scanner.js";
+import QrScanner from "./lib/qr-scanner.min.js";
 
 // Elements
 let startBtn = null;
@@ -6,6 +6,11 @@ let startBtn = null;
 let scanner = null;
 let collectedStamps = [];
 let stamps = [];
+let playIntroduction = false;
+
+let holdingPercentage = 0;
+const holdingTime = 2000;
+const holdingInterval = 100;
 
 document.addEventListener("DOMContentLoaded", init);
 
@@ -14,12 +19,38 @@ async function init() {
     startBtn = document.getElementById("start-btn");
     startBtn.addEventListener("click", loadMain);
 
+    // Get param from URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const startId = urlParams.get("s");
+    if (localStorage.getItem("s") == null && startId) {
+        if (["1", "2"].includes(startId)) {
+            localStorage.setItem("s", startId); // Set the experience ID to 1 or 2 based on the URL parameter
+        } else {
+            localStorage.setItem("s", "0"); // Default to 0 if the user entered the experience from any QR code
+        }
+        playIntroduction = true;
+    }
+
     const savedStamps = localStorage.getItem("stamps");
     if (savedStamps) {
         collectedStamps = JSON.parse(savedStamps);
     }
 
     stamps = await fetchStamps();
+}
+
+/**
+ * Shows the stamp modal, playing the audio and displaying the transcription in real-time.
+ * @param {string} stampId The stamp ID
+ */
+function showStampModal(stampId) {
+    const stamp = stamps.find(s => s.id === stampId);
+
+    if (!stamp) {
+        return;
+    }
+
+    console.log(stamp);
 }
 
 function navigateToPage(page) {
@@ -47,19 +78,68 @@ function collectStamp(stampId) {
 
 function loadMain() {
     navigateToPage("p-main");
+
+    // Init scanner
     startScanner();
+}
+
+function holdQRCode(decodedText) {
+    const urlParams = new URLSearchParams(decodedText);
+    const stampId = urlParams.get("c");
+
+    if (!stampId) {
+        return;
+    }
+
+    holdingPercentage = 0;
+    const interval = setInterval(() => {
+        holdingPercentage += holdingInterval;
+        console.log("Holding percentage:", holdingPercentage);
+        // Make phone vibrate
+        if (navigator.vibrate) {
+            navigator.vibrate(200);
+        }
+        if (holdingPercentage >= holdingTime) {
+            clearInterval(interval);
+            holdingPercentage = 0;
+            showStampModal();
+            collectStamp();
+            console.log("Stamp collected:", stampId);
+        }
+    })
+}
+
+function releaseQRCode(error) {
+    if (error) {
+        console.error("Error releasing QR code:", error);
+    } else {
+        // holdingPercentage = 0;
+    }
 }
 
 function startScanner() {
     if (!scanner) {
-        try {
-            scanner = new Scanner("scanner");
-            scanner.init();
-        } catch (err) {
-            console.error("Error creating scanner: ", err);
-        }
-    } else if (scanner && typeof scanner.stop === "function" && typeof scanner.init === "function") {
+        // try {
+        //     scanner = new Scanner("scanner", holdQRCode, releaseQRCode);
+        //     scanner.init();
+        // } catch (err) {
+        //     console.error("Error creating scanner: ", err);
+        // }
+        scanner = new QrScanner(
+            document.getElementById("scanner"),
+            holdQRCode,
+            {
+                onDecodeError: releaseQRCode,
+                preferredCamera: "environment",
+                highlightScanRegion: true,
+                highlightCodeOutline: true,
+                maxScansPerSecond: 5
+            }
+        )
+        scanner.setInversionMode("invert");
+        scanner.start();
+    } else if (scanner && typeof scanner.stop === "function" && typeof scanner.start === "function") {
         scanner.stop();
-        scanner.init();
+        scanner.start();
     }
 }
